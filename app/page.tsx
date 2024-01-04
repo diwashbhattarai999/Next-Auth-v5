2ac0fe2e96e123907f0167ed8f603ff786ce9ddf
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-04 21:54:22 +0545

    style(ui): complete home page design

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,38 @@
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
 import { Button } from "@/components/ui/button";
 
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 export default function Home() {
-  return <main></main>;
+  return (
+    <main
+      className="
+        flex h-full flex-col items-center justify-center
+        bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-gray-950 via-gray-400 to-gray-950              
+      "
+    >
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-gray-800 drop-shadow-md",
+            font.className
+          )}
+        >
+          🔐 Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentication service</p>
+
+        <div>
+          <Button variant="secondary" size="lg">
+            Sign in
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
 }
