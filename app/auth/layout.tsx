5d3d8556a281b2a0ff117b68d8d384e558f25651
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-05 12:20:56 +0545

    fix: remove html tag causing hydration error

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,15 +1,13 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en">
-      <div
-        className="
+    <div
+      className="
           h-full flex items-center justify-center
           bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-gray-950 via-gray-400 to-gray-950
         "
-      >
-        {children}
-      </div>
-    </html>
+    >
+      {children}
+    </div>
   );
 };
 
