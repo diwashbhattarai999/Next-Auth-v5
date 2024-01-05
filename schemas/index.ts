38a750c1c6f1ceaafe2f9eabc003bd34e68083ae
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-05 18:30:34 +0545

    feat: add register validation schemas

--- schemas/index.ts
+++ schemas/index.ts
@@ -8,3 +8,15 @@ export const LoginSchema = z.object({
     message: "Password is required",
   }),
 });
+
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: "Email is required",
+  }),
+  password: z.string().min(6, {
+    message: "Minimum 6 characters required",
+  }),
+  name: z.string().min(1, {
+    message: "Name is required",
+  }),
+});
