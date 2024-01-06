b6898a8e81e362e0b4142363bbd20aab3100d20f
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-06 17:20:10 +0545

    feat: add pagesurl(signin & error) & emailVerified for oAuths

--- auth.ts
+++ auth.ts
@@ -12,6 +12,19 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error",
+  },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
+    },
+  },
+
   callbacks: {
     async session({ token, session }) {
       if (token.sub && session.user) {
