9d9d220effbc9b69b7b38cb7adbc2bec42d48ccc
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-06 17:22:54 +0545

    feat: add error page

---
+++ app/auth/error/page.tsx
@@ -0,0 +1,11 @@
+import ErrorCard from "@/components/auth/error-card";
+
+const AuthErrorPage = () => {
+    return (
+        <div>
+            <ErrorCard />
+        </div>
+    );
+}
+
+export default AuthErrorPage;
---
+++ components/auth/error-card.tsx
@@ -0,0 +1,18 @@
+import CardWrapper from "@/components/auth/card-wrapper";
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
+
+const ErrorCard = () => {
+  return (
+    <CardWrapper
+      headerLabel="Oops! Something went wrong!"
+      backButtonHref="/auth/login"
+      backButtonLabel="Back to login"
+    >
+      <div className="w-full flex items-center justify-center">
+        <ExclamationTriangleIcon className="text-destructive" />
+      </div>
+    </CardWrapper>
+  );
+};
+
+export default ErrorCard;
