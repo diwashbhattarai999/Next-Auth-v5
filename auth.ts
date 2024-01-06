10e776e138e4e44741fe19b77119c09bff1230fb
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-06 16:03:02 +0545

    feat: complete callbacks for session & jwt

--- auth.ts
+++ auth.ts
@@ -1,8 +1,9 @@
-import NextAuth from "next-auth";
+import NextAuth, { type DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { UserRole } from "@prisma/client";
 
+import { getUserById } from "@/data/user";
 import { db } from "@/lib/db";
-
 import authConfig from "@/auth.config";
 
 export const {
@@ -16,9 +17,22 @@ export const {
       if (token.sub && session.user) {
         session.user.id = token.sub;
       }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
       return session;
     },
     async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
       return token;
     },
   },
