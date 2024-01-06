ed4499f1c92d645819b562d7b34f949ed64a671d
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-06 16:44:28 +0545

    feat: add google and github providers

--- auth.config.ts
+++ auth.config.ts
@@ -3,9 +3,19 @@ import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { LoginSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export default {
   providers: [
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOGGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
     Credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
