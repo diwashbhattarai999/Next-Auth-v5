60aa72a34a7a8b3d93647c36b46a40644c135eb5
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-06 16:45:11 +0545

    feat: add login functionality using github & google

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -3,14 +3,32 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 const Social = () => {
+  const onClick = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
+
   return (
     <div className="flex items-center w-full gap-x-2">
-      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
+      <Button
+        size="lg"
+        className="w-full"
+        variant="outline"
+        onClick={() => onClick("google")}
+      >
         <FcGoogle className="h-5 w-5" />
       </Button>
-      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
+      <Button
+        size="lg"
+        className="w-full"
+        variant="outline"
+        onClick={() => onClick("github")}
+      >
         <FaGithub className="h-5 w-5" />
       </Button>
     </div>
