21725c75b7b55e7250b5c94e5ddc200f85c3fa7c
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-07 09:46:30 +0545

    feat: add send email functionality for reset password

--- actions/reset.ts
+++ actions/reset.ts
@@ -4,6 +4,8 @@ import * as z from "zod";
 
 import { ResetSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
+import { sendPasswordResetEmail } from "@/lib/mail";
+import { generatePasswordResetToken } from "@/lib/tokens";
 
 export const reset = async (values: z.infer<typeof ResetSchema>) => {
   const validatedFields = ResetSchema.safeParse(values);
@@ -20,7 +22,11 @@ export const reset = async (values: z.infer<typeof ResetSchema>) => {
     return { error: "Email not found!" };
   }
 
-  //   TODO: generate token and send email
+  const passwordResetToken = await generatePasswordResetToken(email);
+  await sendPasswordResetEmail(
+    passwordResetToken.email,
+    passwordResetToken.token
+  );
 
   return { success: "Reset email sent!" };
 };
