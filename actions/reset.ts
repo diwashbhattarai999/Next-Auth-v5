8ee35c944daae51b0468a0fa5eae58123a5a4032
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-07 09:02:26 +0545

    feat: add server actions for reset password

---
+++ actions/reset.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import * as z from "zod";
+
+import { ResetSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
+
+export const reset = async (values: z.infer<typeof ResetSchema>) => {
+  const validatedFields = ResetSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid email!" };
+  }
+
+  const { email } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if (!existingUser) {
+    return { error: "Email not found!" };
+  }
+
+  //   TODO: generate token and send email
+
+  return { success: "Reset email sent!" };
+};
