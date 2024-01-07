4d05e2b7bc012cdc838deb833071599099029695
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-07 09:20:30 +0545

    add generate password reset token functionality

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -2,6 +2,30 @@ import { v4 as uuidv4 } from "uuid";
 
 import { getVerificationTokenByEmail } from "@/data/verification-token";
 import { db } from "@/lib/db";
+import { getPasswordResetTokenByEmail } from "@/data/password-token";
+
+export const generatePasswordResetToken = async (email: string) => {
+  const token = uuidv4();
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  const existingToken = await getPasswordResetTokenByEmail(email);
+
+  if (existingToken) {
+    await db.passwordResetToken.delete({
+      where: {
+        id: existingToken.id,
+      },
+    });
+  }
+
+  const passwordResetToken = await db.passwordResetToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  });
+};
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
