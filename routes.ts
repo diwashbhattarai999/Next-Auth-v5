2e3bf20a258e235584dd33dffd5f1607637dfd85
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-07 09:05:52 +0545

    feat: add reset path to authRoutes

--- routes.ts
+++ routes.ts
@@ -10,7 +10,12 @@ export const publicRoutes = ["/", "/auth/new-verification"];
  * These routes will redirect logged in users to /settings
  * @type {string[]}
  */
-export const authRoutes = ["/auth/login", "/auth/register", "/auth/error"];
+export const authRoutes = [
+  "/auth/login",
+  "/auth/register",
+  "/auth/error",
+  "/auth/reset",
+];
 
 /**
  * The prefix for API authentication routes
