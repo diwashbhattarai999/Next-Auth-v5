c268bdb4f488496a1d0737a58a7d6bfa92c3189e
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-07 09:03:39 +0545

    feat add reset validation schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -20,3 +20,9 @@ export const RegisterSchema = z.object({
     message: "Name is required",
   }),
 });
+
+export const ResetSchema = z.object({
+  email: z.string().email({
+    message: "Email is required",
+  }),
+});
