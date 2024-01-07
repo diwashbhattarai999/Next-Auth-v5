f590d103111346312540762ce091c837d3a2577f
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-07 09:31:27 +0545

    feat: add send email functionality for reset password

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,6 +2,17 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+export const sendPasswordResetEmail = async (email: string, token: string) => {
+  const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
+
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: email,
+    subject: "Reset your password",
+    html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
+  });
+};
+
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
 
