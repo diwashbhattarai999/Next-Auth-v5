044ea63d77dc4af9e3b5e7c2291519bc0301873f
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-09 20:36:28 +0545

    feat: show user info using client component

---
+++ app/(protected)/client/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import UserInfo from "@/components/user-info";
+import { useCurrentUser } from "@/hooks/use-current-user";
+
+const ClientPage = () => {
+  const user = useCurrentUser();
+
+  return <UserInfo label="📱 Server Component" user={user} />;
+};
+
+export default ClientPage;
