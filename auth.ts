afec6577e754d2022511b437d8506d2b8bd8178c
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-09 20:31:23 +0545

    feat: add 2FA infor in user's session

--- auth.ts
+++ auth.ts
@@ -60,6 +60,10 @@ export const {
         session.user.role = token.role as UserRole;
       }
 
+      if (session.user) {
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+      }
+
       return session;
     },
     async jwt({ token }) {
@@ -70,6 +74,7 @@ export const {
       if (!existingUser) return token;
 
       token.role = existingUser.role;
+      token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
       return token;
     },
