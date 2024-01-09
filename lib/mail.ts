f8a51726d18aaf922adf36491c0e27aee7679738
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-09 09:43:27 +0545

    feat: add function to send email for 2FA token

--- lib/mail.ts
+++ lib/mail.ts
@@ -23,3 +23,12 @@ export const sendVerificationEmail = async (email: string, token: string) => {
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
   });
 };
+
+export const twoFactorTokenEmail = async (email: string, token: string) => {
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: email,
+    subject: "2FA Code",
+    html: `<p>Your 2FA code: ${token}</p>`,
+  });
+};
