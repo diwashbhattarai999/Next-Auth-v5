3d6e3436b772124c4a33da72ea19a3178b8d9598
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-09 20:37:11 +0545

    feat: show user info using server component

---
+++ app/(protected)/server/page.tsx
@@ -0,0 +1,10 @@
+import UserInfo from "@/components/user-info";
+import { currentUser } from "@/lib/auth";
+
+const ServerPage = async () => {
+  const user = await currentUser();
+
+  return <UserInfo label="🖥️ Server Component" user={user} />;
+};
+
+export default ServerPage;
