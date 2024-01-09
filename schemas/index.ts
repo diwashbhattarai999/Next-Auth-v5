e09a2fab7ec4d1be2f74e72a7cdd2037700f3c5c
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-09 14:07:41 +0545

    feat: add code for 2FA in login schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -7,6 +7,7 @@ export const LoginSchema = z.object({
   password: z.string().min(1, {
     message: "Password is required",
   }),
+  code: z.optional(z.string()),
 });
 
 export const RegisterSchema = z.object({
