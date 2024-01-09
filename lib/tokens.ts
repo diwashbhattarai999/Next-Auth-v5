ce00aba409068460abd2d50e316a0047535ccb14
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-09 09:43:54 +0545

    feat: add function to generate 2FA token

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,8 +1,10 @@
+import crypto from "crypto";
 import { v4 as uuidv4 } from "uuid";
 
 import { getVerificationTokenByEmail } from "@/data/verification-token";
 import { db } from "@/lib/db";
 import { getPasswordResetTokenByEmail } from "@/data/password-token";
+import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
 
 export const generatePasswordResetToken = async (email: string) => {
   const token = uuidv4();
@@ -52,4 +54,31 @@ export const generateVerificationToken = async (email: string) => {
   });
 
   return verificationToken;
+};
+
+export const generateTwoFactorToken = async (email: string) => {
+  const token = crypto.randomInt(100_000, 1_000_000).toString();
+
+  // TODO: Later change to 15 minutes
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  const existingToken = await getTwoFactorTokenByEmail(email);
+
+  if (existingToken) {
+    await db.twoFactorToken.delete({
+      where: {
+        id: existingToken.id,
+      },
+    });
+  }
+
+  const twoFactorToken = await db.twoFactorToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  });
+
+  return twoFactorToken;
 };
