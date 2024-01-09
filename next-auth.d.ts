4b3864aebe903ffd6edb33b3493f5a7117fcbcda
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-09 20:33:09 +0545

    feat: add is2FA type in user

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -3,6 +3,7 @@ import { DefaultSession } from "next-auth";
 
 export type ExtendedUser = DefaultSession["user"] & {
   role: UserRole;
+  isTwoFactorEnabled: boolean;
 };
 
 declare module "next-auth" {
