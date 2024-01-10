2755c911d421d232629b2a2d7a36025ee5a98a81
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-10 09:35:52 +0545

    fix: change label from server to client

--- app/(protected)/client/page.tsx
+++ app/(protected)/client/page.tsx
@@ -6,7 +6,7 @@ import { useCurrentUser } from "@/hooks/use-current-user";
 const ClientPage = () => {
   const user = useCurrentUser();
 
-  return <UserInfo label="📱 Server Component" user={user} />;
+  return <UserInfo label="📱 Client Component" user={user} />;
 };
 
 export default ClientPage;
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,3 +1,4 @@
+import { UserRole } from "@prisma/client";
 import * as z from "zod";
 
 export const LoginSchema = z.object({
@@ -33,3 +34,39 @@ export const NewPasswordSchema = z.object({
     message: "Minimum 6 characters required!",
   }),
 });
+
+export const SettingsSchema = z
+  .object({
+    name: z.optional(z.string()),
+    isTwoFactorEnabled: z.optional(z.boolean()),
+    role: z.enum([UserRole.ADMIN, UserRole.USER]),
+    email: z.optional(z.string().email()),
+    password: z.optional(z.string().min(6)),
+    newPassword: z.optional(z.string().min(6)),
+  })
+  .refine(
+    (data) => {
+      if (data.password && !data.newPassword) {
+        return false;
+      }
+
+      return true;
+    },
+    {
+      message: "New password is required!",
+      path: ["newPassword"],
+    }
+  )
+  .refine(
+    (data) => {
+      if (data.newPassword && !data.password) {
+        return false;
+      }
+
+      return true;
+    },
+    {
+      message: "Password is required!",
+      path: ["password"],
+    }
+  );
