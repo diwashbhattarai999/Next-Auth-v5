22631cdf133c551bb9a7798a87ac425a650fc812
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-10 09:32:25 +0545

    feat: add isOAuth type in extended user

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -4,6 +4,7 @@ import { DefaultSession } from "next-auth";
 export type ExtendedUser = DefaultSession["user"] & {
   role: UserRole;
   isTwoFactorEnabled: boolean;
+  isOAuth: boolean;
 };
 
 declare module "next-auth" {
