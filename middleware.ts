875ba2f0207fb15ba37f787188159059a3a01d11
Diwash Bhattarai <diwashb999@gmail.com>
2024-01-10 10:01:41 +0545

    feat: add function for callbackUrl

--- middleware.ts
+++ middleware.ts
@@ -28,7 +28,16 @@ export default auth((req) => {
   }
 
   if (!isLoggedIn && !isPublicRoute) {
-    return Response.redirect(new URL("/auth/login", nextUrl));
+    let callbackUrl = nextUrl.pathname;
+    if (nextUrl.search) {
+      callbackUrl += nextUrl.search;
+    }
+
+    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+
+    return Response.redirect(
+      new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`, nextUrl)
+    );
   }
 });
 
